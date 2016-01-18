ce9f70a7fd7573373c6b78ea4402639cb2964f1f
Prabin Karanjit <pkaranjit@gmail.com>
2016-01-17 19:10:48 -0500

    final wrap

--- web.js
+++ web.js
@@ -1,14 +1,8 @@
-// web.js
 var gzippo = require('gzippo');
-var express = require("express");
-var logfmt = require("logfmt");
+var express = require('express');
+var morgan = require('morgan');
 var app = express();
 
-app.use(logfmt.requestLogger());
-
-//app.use(express.static(__dirname + '/dist'));
+app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-     console.log("Listening on " + port);
-});
+app.listen(process.env.PORT || 5000);
